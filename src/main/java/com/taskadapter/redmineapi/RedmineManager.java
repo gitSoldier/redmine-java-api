45adb1d1fe4c5c562e0231095c2124184a21cb6b
Wesley Falcao <wesfalc@yahoo.com>
2013-08-09 15:31:06 -0400

    test commit 01

--- src/main/java/com/taskadapter/redmineapi/RedmineManager.java
+++ src/main/java/com/taskadapter/redmineapi/RedmineManager.java
@@ -13,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-
+// test modification 01
 package com.taskadapter.redmineapi;
 
 import java.io.ByteArrayInputStream;
